5ad3754139156eda22a15bad883f1ebaa76d41b4
artursdrosa <artursr22@hotmail.com>
2026-07-14 00:24:48 -0300

    Criado a função get_db

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from decouple import config
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{config('DB_USER')}:"
@@ -12,4 +12,14 @@ DATABASE_URL = (
 
 engine = create_engine(DATABASE_URL)
 
+SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
