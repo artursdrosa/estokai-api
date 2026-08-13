3b016c755cc4eb54a8b2f0181e9152cb2124b9d4
artursdrosa <artursr22@hotmail.com>
2026-08-13 00:30:33 -0300

    Atualizando schema do produto

--- schemas/produto.py
+++ schemas/produto.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class ProdutoBase(BaseModel):
@@ -21,5 +21,4 @@ class ProdutoUpdate(BaseModel):
 class ProdutoRead(ProdutoBase):
     id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
